e76511129fc7c82640a711397332b9ff94cf04db
ole <ole@pixum.com>
2023-04-23 18:48:44 +0200

    chore: install symfony (v6.2.9) api platform distribution

---
+++ pwa/components/admin/DocContext.ts
@@ -0,0 +1,9 @@
+import {createContext} from 'react';
+
+const DocContext = createContext({
+  docType: 'hydra',
+  setDocType: (_docType: string) => {
+  },
+});
+
+export default DocContext;
---
+++ pwa/pages/books/index.tsx
@@ -0,0 +1,30 @@
+import {GetStaticProps} from "next";
+import {dehydrate, QueryClient} from "react-query";
+
+import {
+  PageList,
+  getBooks,
+  getBooksPath,
+} from "../../components/book/PageList";
+import {ENTRYPOINT} from "../../config/entrypoint";
+
+export const getStaticProps: GetStaticProps = async () => {
+  // prevent failure on build without API available
+  if (!ENTRYPOINT) {
+    return {
+      props: {},
+    };
+  }
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(getBooksPath(), getBooks());
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: 1,
+  };
+};
+
+export default PageList;
